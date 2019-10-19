f29c4eaf5c0ecc76cee8d7b320e730dd28528068
Danilo Nikolic <nikolicdanilo1410@gmail.com>
2019-10-18 17:12:07 -0700

    late

--- main.js
+++ main.js
@@ -7,27 +7,27 @@ $(document).ready(function() {
   })
 
   //**********ainhoa***********l
-  
-$.get("https://obscure-tundra-54269.herokuapp.com/fine-dining").done(function(data){
-  console.log(data["appetizers"])
 
+  $.get("https://obscure-tundra-54269.herokuapp.com/fine-dining").done(function(
+    data
+  ) {
+    console.log(data["appetizers"])
 
     $("#sec1").on("click", "p", function() {
-      
- $(".dinner").remove()
+      $(".dinner").remove()
 
       const type = $(this).html()
       console.log(data["appetizers"][0].price)
       console.log(type.toLowerCase())
-    let dat=data[type.toLowerCase()].map(d =>{
-      console.log(d.price)
+      let dat = data[type.toLowerCase()]
+        .map(d => {
+          console.log(d.price)
 
-      console.log(d.name)
-      console.log(d.description)
-      console.log(d.extra.glutenfree)
+          console.log(d.name)
+          console.log(d.description)
+          console.log(d.extra.glutenfree)
 
-
-      return `
+          return `
       <div class="dinner">
         <div class="menuline1">
           <p id="name">${d.name}</p>
@@ -40,26 +40,13 @@ $.get("https://obscure-tundra-54269.herokuapp.com/fine-dining").done(function(da
             <li>${d.extra.vegetarian}<i class="fa fa-check"></i></li>
           </ul>
         </div>
-      ` 
-    }).join('')
+      `
+        })
+        .join("")
 
-    $(".menu").append(dat)
-
-    
-
-
+      $(".menu").append(dat)
+    })
   })
 
-
-
-})
-
-  
   //***********ainhoa**********
-
-
-
-
-
-
 })
